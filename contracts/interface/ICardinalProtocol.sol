a2265db2769d55b9a6a4b45b12108d09a4a6e12a
harpoonjs <w3st.io2021@gmail.com>
2022-12-20 06:57:13 -0500

    Remove unused interface

--- contracts/interface/ICardinalProtocol.sol
+++ contracts/interface/ICardinalProtocol.sol
@@ -4,13 +4,13 @@ pragma solidity ^0.8.9;
 
 
 /* ========== [IMPORT] ========== */
-// // @openzeppelin/contracts/token
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 // // @openzeppelin/contracts/access
 import "@openzeppelin/contracts/access/IAccessControlEnumerable.sol";
 
 
-interface ICardinalProtocol is IERC20, IAccessControlEnumerable {
+interface ICardinalProtocol is
+	IAccessControlEnumerable
+{
 	function authLevel_admin(address account) external view returns (bool);
 
 	function authLevel_chief(address account) external view returns (bool);
